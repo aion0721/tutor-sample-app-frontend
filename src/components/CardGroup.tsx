1b41411c26167aeb7ae284208bb9c8a95d2b2597
aion0721 <rudolf.aion@gmail.com>
2025-07-23 10:52:59 +0900

    fix: empty state

--- src/components/CardGroup.tsx
+++ src/components/CardGroup.tsx
@@ -1,5 +1,6 @@
 import {
   ButtonGroup,
+  EmptyState,
   HStack,
   IconButton,
   Pagination,
@@ -8,6 +9,7 @@ import {
 import { useState } from "react";
 import { HiChevronLeft, HiChevronRight } from "react-icons/hi";
 import { CardComponent } from "./Card";
+import { TbCircleDotted } from "react-icons/tb";
 
 const pageSize = 5;
 
@@ -29,39 +31,54 @@ const SampleGrid = ({ items }: SampleGridProps) => {
 
   return (
     <VStack gap={4}>
-      <HStack>
-        {visibleItems.map((item, index) => (
-          <CardComponent key={index} {...item} />
-        ))}
-      </HStack>
-      <Pagination.Root
-        count={items.length}
-        pageSize={pageSize}
-        page={page}
-        onPageChange={(e) => setPage(e.page)}
-      >
-        <ButtonGroup variant="ghost" size="sm">
-          <Pagination.PrevTrigger asChild>
-            <IconButton>
-              <HiChevronLeft />
-            </IconButton>
-          </Pagination.PrevTrigger>
+      {items.length == 0 ? (
+        <EmptyState.Root>
+          <EmptyState.Content>
+            <EmptyState.Indicator>
+              <TbCircleDotted />
+            </EmptyState.Indicator>
+            <VStack textAlign="center">
+              <EmptyState.Title>Empty</EmptyState.Title>
+            </VStack>
+          </EmptyState.Content>
+        </EmptyState.Root>
+      ) : (
+        <>
+          <HStack>
+            {visibleItems.map((item, index) => (
+              <CardComponent key={index} {...item} />
+            ))}
+          </HStack>
+          <Pagination.Root
+            count={items.length}
+            pageSize={pageSize}
+            page={page}
+            onPageChange={(e) => setPage(e.page)}
+          >
+            <ButtonGroup variant="ghost" size="sm">
+              <Pagination.PrevTrigger asChild>
+                <IconButton>
+                  <HiChevronLeft />
+                </IconButton>
+              </Pagination.PrevTrigger>
 
-          <Pagination.Items
-            render={(page) => (
-              <IconButton variant={{ base: "ghost", _selected: "outline" }}>
-                {page.value}
-              </IconButton>
-            )}
-          />
+              <Pagination.Items
+                render={(page) => (
+                  <IconButton variant={{ base: "ghost", _selected: "outline" }}>
+                    {page.value}
+                  </IconButton>
+                )}
+              />
 
-          <Pagination.NextTrigger asChild>
-            <IconButton>
-              <HiChevronRight />
-            </IconButton>
-          </Pagination.NextTrigger>
-        </ButtonGroup>
-      </Pagination.Root>
+              <Pagination.NextTrigger asChild>
+                <IconButton>
+                  <HiChevronRight />
+                </IconButton>
+              </Pagination.NextTrigger>
+            </ButtonGroup>
+          </Pagination.Root>
+        </>
+      )}
     </VStack>
   );
 };
