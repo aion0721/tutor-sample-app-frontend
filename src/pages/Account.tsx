faa6256c2db8c9e649f98fdebe8b4152ea73e5b5
aion0721 <rudolf.aion@gmail.com>
2025-07-23 22:00:42 +0900

    fix: account form

--- src/pages/Account.tsx
+++ src/pages/Account.tsx
@@ -2,21 +2,38 @@ import { Box, Heading, Stack, Input, Button, Field } from "@chakra-ui/react";
 import { useForm } from "react-hook-form";
 import { Toaster, toaster } from "@/components/ui/toaster";
 import { PasswordInput } from "@/components/ui/password-input";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-interface FormValues {
-  username: string;
-  mailaddress: string;
-  password: string;
-}
+const schema = z.object({
+  username: z.string().min(1, { message: "Username を入力してね" }),
+
+  mailaddress: z.email({
+    message: "メールアドレスの形式が変かも💦",
+  }),
+
+  password: z.string().min(8, { message: "パスワードは 8 文字以上にしてね" }),
+});
+
+type FormValues = z.infer<typeof schema>;
 
 export const Account = () => {
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<FormValues>();
+  } = useForm<FormValues>({
+    resolver: zodResolver(schema),
+    mode: "onBlur",
+  });
 
-  const onSubmit = handleSubmit((data) => toaster.create({ title: "create" }));
+  const onSubmit = handleSubmit((data) =>
+    toaster.create({
+      title: "create",
+      description: `${data.username}`,
+      type: "success",
+    })
+  );
 
   return (
     <Box
